cfc085cf36fef2a4b4b504a800d4b47e11fe87fd
Leah Cardoz <leahcardoz@gmail.com>
2023-08-24 17:07:59 -0400

    working on navbar, need to fix sublinks css

--- app/public/wp-content/themes/theatre-three-theme/header.php
+++ app/public/wp-content/themes/theatre-three-theme/header.php
@@ -11,12 +11,67 @@
         <h1 class="title-font"><a href="<?php echo site_url('/')?>">Theatre<i>Three</i></a></h1>
         <p class="subtitle-font">Broadway on Main Street</p>
       </div>
-      <div class="nav-bar">
-        <a href="<?php echo site_url('/tickets')?>">Tickets</a>
+      <nav class="nav-bar">
+        <ul class="nav-list">
+          <li class="nav-item">
+            <a href="<?php echo site_url('/tickets')?>" class="nav-link">Tickets</a>
+            <ul class="sub-links">
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Season</a>
+            <ul class="sub-links">
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Engage & Learn</a>
+            <ul class="sub-links">
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Your Visit</a>
+            <ul class="sub-links">
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="<?php echo site_url('/about-us')?>" class="nav-link">About Us</a>            <ul class="sub-links">
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+            </ul>
+          </li>
+          <li class="nav-item">
+            <a href="#" class="nav-link">Support Us</a>
+            <ul class="sub-links">
+              <li>
+                <a href="<?php echo site_url('/tickets')?>">Buy Tickets</a>
+              </li>
+            </ul>
+          </li>
+        </ul>
+        <!-- <a href="<?php echo site_url('/tickets')?>">Tickets</a>
         <a href="#">Season</a>
         <a href="#">Engage & Learn</a>
         <a href="#">Your Visit</a>
         <a href="<?php echo site_url('/about-us')?>">About Us</a>
-        <a href="#">Support Us</a>
-      </div>
+        <a href="#">Support Us</a> -->
+      </nav>
     </header>
