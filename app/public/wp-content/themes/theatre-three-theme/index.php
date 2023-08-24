f07ed0908c5b66c9e88466957df4b22a37e59e16
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-23 21:24:34 -0400

    oops

--- app/public/wp-content/themes/theatre-three-theme/index.php
+++ app/public/wp-content/themes/theatre-three-theme/index.php
@@ -1,10 +1,31 @@
 <?php get_header(); ?>
-
-<div>
-
-  <h1></h1>
-
+<div class="div-container">
+  <div class="div-one">
+    <h1>Block 1</h1>
+  </div>
+  <div class="div-two">
+    <h1><a href="<?php echo site_url('/tickets')?>">Tickets</a></h1>
+  </div>
 </div>
+
+<div class="div-container-two">
+  <div class="div-three">
+    <h1>Block 3</h1>
+  </div>
+  <div class="div-four">
+    <h1>Block 4</h1>
+  </div>
+</div>
+
+<div class="div-container-three">
+  <div class="div-five">
+    <h1>Block 5</h1>
+  </div>
+  <div class="div-six">
+    <h1>Block 6</h1>
+  </div>
+</div>
+
 <div class="container container--narrow page-section">
   <!-- <?php
     while(have_posts()){
