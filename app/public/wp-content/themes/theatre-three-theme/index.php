8bb3509842f9f7617f4cff7a88e2742b15fc4b9f
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-25 12:13:35 -0400

    deleted unnecessary files

--- app/public/wp-content/themes/theatre-three-theme/index.php
+++ app/public/wp-content/themes/theatre-three-theme/index.php
@@ -14,29 +14,6 @@
     <h1 class="div-two-text"><a href="<?php echo site_url('#')?>">View our Calendar</a></h1>
   </div>
 </div>
-
-<div class="container container--narrow page-section">
-  <!-- <?php
-    while(have_posts()){
-      the_post(); ?>
-      <div class="post-item">
-        <h2><a class="headline headline--medium headline--post-title" href="<?php the_permalink();?>"><?php the_title();?></a></h2>
-        <div class="metabox">
-          <p>Posted by <?php the_author_posts_link();?> on <?php the_time('M j, Y');?> in <?php echo get_the_category_list(',');?></p>
-        </div>
-
-        <div class="generic-content">
-          <?php the_excerpt();?>
-          <p><a class="btn btn--blue" href="<?php the_permalink();?>">Continue Reading &raquo;</a></p>
-        </div>
-      </div>
-    <?php }
-    echo paginate_links();
-  ?> -->
-</div>
-
-
 <?php
 get_footer();
-
 ?>
