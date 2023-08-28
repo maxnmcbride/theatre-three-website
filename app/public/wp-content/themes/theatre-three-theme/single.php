4542717137f5459d6193836b6526dde268ed483e
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-28 11:03:16 -0400

    custom post types working

--- app/public/wp-content/themes/theatre-three-theme/single.php
+++ app/public/wp-content/themes/theatre-three-theme/single.php
@@ -7,6 +7,7 @@ if (have_posts()) {
     while (have_posts()) {
         the_post(); ?>
 <h1 class="post-title"><?php the_title();?></h1>
+<h2 class="generic-post-content">this is from the single.php file</h2>
 <div class="generic-post-content"><?php the_content();?></div>
 <div class="post-image-container">
 <div class="post-image">
