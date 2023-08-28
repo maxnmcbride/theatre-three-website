202c04f6dbccdb4f03bcfa4bc3f1a17e2cf7f866
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-28 11:03:16 -0400

    custom post types working

--- app/public/wp-content/themes/theatre-three-theme/index.php
+++ app/public/wp-content/themes/theatre-three-theme/index.php
@@ -9,11 +9,45 @@
 <div class="div-container">
   <div class="div-one">
     <h1 class="div-one-text">Upcoming Shows</h1>
-  </div>
-  <div class="div-two">
+
+    <!-- this is querying the show data from the custom post type of shows -->
+<?php
+
+$homepageShows = new WP_Query(array(
+  'posts_per_page' => -1,
+  'post_type' => 'show'
+));
+
+while($homepageShows->have_posts()){
+  $homepageShows->the_post(); ?>
+    <div>
+      <a href="<?php the_permalink();?>" class="div-one-text">
+      <li><?php the_title(); ?></li></a>
+      <div>
+        <span class="div-one-text"><?php 
+          $startDate = new DateTime(get_field('start_date'));
+          echo $startDate->format('M')
+        ?></span>
+        <span class="div-one-text"><?php 
+         $endDate = new DateTime(get_field('end_date'));
+         echo $endDate->format('M')
+        ?></span>
+      </div>
+    </div>
+
+<?php }
+
+?>
+<a class="div-one-text"></a>
+</div>
+  
+
+<div class="div-two">
     <h1 class="div-two-text"><a href="<?php echo site_url('#')?>">View our Calendar</a></h1>
   </div>
 </div>
+
+
 <?php
 get_footer();
 ?>
