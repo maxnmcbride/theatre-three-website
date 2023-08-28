a7f15919691358b2af4c659166f8088cf963ca97
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-28 12:57:31 -0400

    updated logic for displaying shows. fixed formatting issues

--- app/public/wp-content/themes/theatre-three-theme/index.php
+++ app/public/wp-content/themes/theatre-three-theme/index.php
@@ -11,42 +11,60 @@
     <h1 class="div-one-text">Upcoming Shows</h1>
 
     <!-- this is querying the show data from the custom post type of shows -->
-<?php
+    <!-- this logic is handling displaying shows that end after todays date -->
+    <?php
+    $today = date('Ymd');
+    $homepageShows = new WP_Query(
+      array(
+        'posts_per_page' => -1,
+        'post_type' => 'show',
+        'meta_key' => 'start_date',
+        'orderby' => 'meta_value_num',
+        'order' => 'ASC',
+        'meta_query' => array(
+          array(
+            'key' => 'end_date',
+            'compare' => '>=',
+            'value' => $today,
+            'type' => 'numeric'
+          )
+        )
+      )
+    );
 
-$homepageShows = new WP_Query(array(
-  'posts_per_page' => -1,
-  'post_type' => 'show',
-  'meta_key' => 'start_date',
-  'orderby' => 'meta_value_num',
-  'order' => 'ASC'
-));
-
-while($homepageShows->have_posts()){
-  $homepageShows->the_post(); ?>
-    <div>
-      <a href="<?php the_permalink();?>" class="div-one-text">
-      <li><?php the_title(); ?></li></a>
+    while ($homepageShows->have_posts()) {
+      $homepageShows->the_post(); ?>
       <div>
-        <span class="div-one-text"><?php 
-          $startDate = new DateTime(get_field('start_date'));
-          echo $startDate->format('M Y');
-        ?></span>
-        <span class="div-one-text"><?php 
-         $endDate = new DateTime(get_field('end_date'));
-         echo $endDate->format('M Y');
-        ?></span>
+        <a href="<?php the_permalink(); ?>" class="div-one-text">
+          <li>
+            <?php the_title(); ?>
+          </li>
+        </a>
+        <div>
+          <span class="div-one-text">
+            <?php
+            $startDate = new DateTime(get_field('start_date'));
+            echo $startDate->format('M Y');
+            ?>
+          </span>
+          <span class="div-one-text">
+            <?php
+            $endDate = new DateTime(get_field('end_date'));
+            echo $endDate->format('M Y');
+            ?>
+          </span>
+        </div>
       </div>
-    </div>
 
-<?php }
+    <?php }
 
-?>
-<a class="div-one-text"></a>
-</div>
-  
+    ?>
+    <a class="div-one-text"></a>
+  </div>
 
-<div class="div-two">
-    <h1 class="div-two-text"><a href="<?php echo site_url('#')?>">View our Calendar</a></h1>
+
+  <div class="div-two">
+    <h1 class="div-two-text"><a href="<?php echo site_url('#') ?>">View our Calendar</a></h1>
   </div>
 </div>
 
--- app/public/wp-content/themes/theatre-three-theme/single.php
+++ app/public/wp-content/themes/theatre-three-theme/single.php
@@ -6,17 +6,21 @@ get_header();
 if (have_posts()) {
     while (have_posts()) {
         the_post(); ?>
-<h1 class="post-title"><?php the_title();?></h1>
-<h2 class="generic-post-content">this is from the single.php file</h2>
-<div class="generic-post-content"><?php the_content();?></div>
-<div class="post-image-container">
-<div class="post-image">
-    <?php
-        if (has_post_thumbnail()) {
-            the_post_thumbnail('large');
-        }
+        <h1 class="post-title">
+            <?php the_title(); ?>
+        </h1>
+        <h2 class="generic-post-content">this is from the single.php file</h2>
+        <div class="generic-post-content">
+            <?php the_content(); ?>
+        </div>
+        <div class="post-image-container">
+            <div class="post-image">
+                <?php
+                if (has_post_thumbnail()) {
+                    the_post_thumbnail('large');
+                }
     }
 }
 ?>
-</div>
+    </div>
 </div>
