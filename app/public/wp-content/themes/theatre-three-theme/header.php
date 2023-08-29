475b6dbc0933ac53b2128a52e0a49e8ef3f5e4de
Leah Cardoz <leahcardoz@gmail.com>
2023-08-29 15:04:24 -0400

    progress on navbar css, added transition and hover over to sublinks

--- app/public/wp-content/themes/theatre-three-theme/header.php
+++ app/public/wp-content/themes/theatre-three-theme/header.php
@@ -22,10 +22,10 @@
               <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
             </li>
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Seating Chart</a>
             </li>
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Ticket Policy</a>
             </li>
           </ul>
         </li>
@@ -33,7 +33,7 @@
           <a href="#" class="nav-link">Season</a>
           <ul class="sub-links">
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Events Calendar</a>
             </li>
           </ul>
         </li>
@@ -41,7 +41,7 @@
           <a href="#" class="nav-link">Engage & Learn</a>
           <ul class="sub-links">
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Acting Classes</a>
             </li>
           </ul>
         </li>
@@ -49,7 +49,7 @@
           <a href="#" class="nav-link">Your Visit</a>
           <ul class="sub-links">
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Plan a Tour</a>
             </li>
           </ul>
         </li>
@@ -57,7 +57,7 @@
           <a href="<?php echo site_url('/about-us') ?>" class="nav-link">About Us</a>
           <ul class="sub-links">
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Theatre History</a>
             </li>
           </ul>
         </li>
@@ -65,7 +65,7 @@
           <a href="#" class="nav-link">Support Us</a>
           <ul class="sub-links">
             <li>
-              <a href="<?php echo site_url('/tickets') ?>">Buy Tickets</a>
+              <a href="<?php echo site_url('/tickets') ?>">Donate</a>
             </li>
           </ul>
         </li>
