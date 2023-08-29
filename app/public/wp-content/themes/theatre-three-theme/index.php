3128d458c3d58b7d597ba416f114465d05aa74f7
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-08-29 15:08:00 -0400

    refactored mainstage shows into its own component, need to build out WP CPT for second stage, childrens stage, and acting classes still

--- app/public/wp-content/themes/theatre-three-theme/index.php
+++ app/public/wp-content/themes/theatre-three-theme/index.php
@@ -5,71 +5,184 @@
     <h1 class="main-div-text">Explore Theatre Three</h1>
   </div>
 </div>
+<h1 class="div-one-text">Upcoming Events</h1>
 
-<div class="div-container">
-  <div class="div-one">
-    <h1 class="div-one-text">Upcoming Shows</h1>
+<div class="main-div-events-container">
+  <div class="main-div-events-mainstage-shows">
+    <?php get_template_part('mainstage-shows-component'); ?>
+  </div>
 
-    <!-- this is querying the show data from the custom post type of shows -->
-    <!-- this logic is handling displaying shows that end after todays date -->
-    <div>
-    <h2 class="upcoming-stage-titles"><a href="<?php echo get_post_type_archive_link('show')?>">Mainstage Shows</a></h2>
-    <?php
-    $today = date('Ymd');
-    $homepageShows = new WP_Query(
-      array(
-        'posts_per_page' => 3,
-        'post_type' => 'show',
-        'meta_key' => 'start_date',
-        'orderby' => 'meta_value_num',
-        'order' => 'ASC',
-        'meta_query' => array(
-          array(
-            'key' => 'end_date',
-            'compare' => '>=',
-            'value' => $today,
-            'type' => 'numeric'
+  <div class="main-div-events-secondstage-shows">
+
+    <div class="div-one">
+      <h2 class="upcoming-stage-titles"><a href="<?php echo get_post_type_archive_link('show') ?>">Second Stage
+          Shows</a>
+      </h2>
+      <?php
+      $today = date('Ymd');
+      $homepageShows = new WP_Query(
+        array(
+          'posts_per_page' => 3,
+          'post_type' => 'show',
+          'meta_key' => 'start_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'end_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
           )
         )
-      )
-    );
+      );
 
-    while ($homepageShows->have_posts()) {
-      $homepageShows->the_post(); ?>
-      <div>
-        <h3 class="upcoming-show-titles">
-        <a href="<?php the_permalink(); ?>">
-            <?php the_title(); ?>
-          </a>
-    </h3>
+      while ($homepageShows->have_posts()) {
+        $homepageShows->the_post(); ?>
         <div>
-          <span class="div-one-text">
-            <?php
-            $startDate = new DateTime(get_field('start_date'));
-            echo $startDate->format('M Y');
-            ?>
-          </span>
-          <span class="div-one-text">
-            <?php
-            $endDate = new DateTime(get_field('end_date'));
-            echo $endDate->format('M Y');
-            ?>
-          </span>
+          <h3 class="upcoming-show-titles">
+            <a href="<?php the_permalink(); ?>">
+              <?php the_title(); ?>
+            </a>
+          </h3>
+          <div>
+            <span class="div-one-text">
+              <?php
+              $startDate = new DateTime(get_field('start_date'));
+              echo $startDate->format('M Y');
+              ?>
+            </span>
+            <span class="div-one-text">
+              <?php
+              $endDate = new DateTime(get_field('end_date'));
+              echo $endDate->format('M Y');
+              ?>
+            </span>
+          </div>
         </div>
-      </div>
 
-    <?php }
+      <?php }
 
-    ?>
+      ?>
     </div>
 
-    <a class="div-one-text"></a>
   </div>
 
+  <div class="main-div-events-childrens-theater-shows">
 
-  <div class="div-two">
-    <h1 class="div-two-text"><a href="<?php echo site_url('#') ?>">View our Calendar</a></h1>
+    <div class="div-one">
+      <h2 class="upcoming-stage-titles"><a href="<?php echo get_post_type_archive_link('show') ?>">Children's
+          Theatre</a>
+      </h2>
+      <?php
+      $today = date('Ymd');
+      $homepageShows = new WP_Query(
+        array(
+          'posts_per_page' => 3,
+          'post_type' => 'show',
+          'meta_key' => 'start_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'end_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          )
+        )
+      );
+
+      while ($homepageShows->have_posts()) {
+        $homepageShows->the_post(); ?>
+        <div>
+          <h3 class="upcoming-show-titles">
+            <a href="<?php the_permalink(); ?>">
+              <?php the_title(); ?>
+            </a>
+          </h3>
+          <div>
+            <span class="div-one-text">
+              <?php
+              $startDate = new DateTime(get_field('start_date'));
+              echo $startDate->format('M Y');
+              ?>
+            </span>
+            <span class="div-one-text">
+              <?php
+              $endDate = new DateTime(get_field('end_date'));
+              echo $endDate->format('M Y');
+              ?>
+            </span>
+          </div>
+        </div>
+
+      <?php }
+
+      ?>
+    </div>
+
   </div>
+
+  <div class="main-div-events-acting-classes">
+
+    <div class="div-one">
+      <h2 class="upcoming-stage-titles"><a href="<?php echo get_post_type_archive_link('show') ?>">Acting Classes</a>
+      </h2>
+      <?php
+      $today = date('Ymd');
+      $homepageShows = new WP_Query(
+        array(
+          'posts_per_page' => 3,
+          'post_type' => 'show',
+          'meta_key' => 'start_date',
+          'orderby' => 'meta_value_num',
+          'order' => 'ASC',
+          'meta_query' => array(
+            array(
+              'key' => 'end_date',
+              'compare' => '>=',
+              'value' => $today,
+              'type' => 'numeric'
+            )
+          )
+        )
+      );
+
+      while ($homepageShows->have_posts()) {
+        $homepageShows->the_post(); ?>
+        <div>
+          <h3 class="upcoming-show-titles">
+            <a href="<?php the_permalink(); ?>">
+              <?php the_title(); ?>
+            </a>
+          </h3>
+          <div>
+            <span class="div-one-text">
+              <?php
+              $startDate = new DateTime(get_field('start_date'));
+              echo $startDate->format('M Y');
+              ?>
+            </span>
+            <span class="div-one-text">
+              <?php
+              $endDate = new DateTime(get_field('end_date'));
+              echo $endDate->format('M Y');
+              ?>
+            </span>
+          </div>
+        </div>
+
+      <?php }
+
+      ?>
+    </div>
+
+  </div>
+</div>
+
 </div>
 
 
