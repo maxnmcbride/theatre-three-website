528556330877ebd0fbd5eef410e6f4393f0f7dbd
Leah Cardoz <leahcardoz@gmail.com>
2023-08-31 12:53:44 -0400

    imported more google font styles

--- app/public/wp-content/themes/theatre-three-theme/header.php
+++ app/public/wp-content/themes/theatre-three-theme/header.php
@@ -8,7 +8,7 @@
   <!-- imported custom google fonts: -->
   <link rel="preconnect" href="https://fonts.googleapis.com">
   <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,200;0,400;1,200;1,400&display=swap" rel="stylesheet"> 
 </head>
 
 <body class="body" <?php body_class(); ?>>
