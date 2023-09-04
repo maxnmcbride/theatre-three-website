c52230e9e22343a744c0d6d96ce06db7d5b889c3
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-09-04 18:57:25 -0400

    logic figured out for showing cast images with dynamic array

--- app/public/wp-content/themes/theatre-three-theme/single-second_stage_show.php
+++ app/public/wp-content/themes/theatre-three-theme/single-second_stage_show.php
@@ -1,18 +1,30 @@
 <?php
 get_header();
 ?>
-
-<h1 class="post-title"><?php 
- echo get_acf_field_count();
-?></h1>
+<h1 class="post-title">THIS IS MADE BY THE SINGLE SECOND STAGE SHOW</h1>
 
 <div class="show-layout">
+    <div class="">
     <?php
     if (have_posts()) {
         while (have_posts()) {
             the_post();
+
             // Get the image URL from the ACF field
-            $image = get_field('second_stage_image');
+            $image = get_field('show_image');
+            $castImages = array();
+
+            // Loop through the cast_image_X fields
+            for ($i = 1; ; $i++) {
+                $castImage = get_field('cast_image_' . $i);
+
+                if ($castImage) {
+                    $castImages[] = $castImage;
+                } else {
+                    // Break the loop if no more images found
+                    break;
+                }
+            }
             ?>
 
             <div class="show-header">
@@ -37,16 +49,55 @@ get_header();
             </div>
 
             <div class="show-content">
+                <h2 class="post-title">About the Show</h2>
                 <div class="generic-post-content">
                     <?php the_content(); ?>
                 </div>
             </div>
 
             <div class="show-metabox">
-                <!-- Your metabox content will go here -->
+                <h2 class="post-title">Ticket Information</h2>
+                <div class="metabox-ticket-information-header">
+                    <?php
+                    $ticket_information = get_field('show_ticket_information');
+                    if ($ticket_information) {
+                        ?>
+                        <div class="metabox-ticket-information-container">
+                            <p class="metabox-ticket-information-text">
+                                <?php echo htmlspecialchars_decode($ticket_information) ?>
+                            </p>
+                        </div>
+                        <?php
+                    }
+                    ?>
+                </div>
             </div>
 
-            <?php
+            <div class="second-stage-cast-images">
+                <?php
+                foreach ($castImages as $index => $castImage) {
+                    $castImage_url = $castImage['url'];
+                    $castImage_alt = $castImage['alt'];
+                    $castImage_caption = $castImage['caption'];
+                    $castImage_description = $castImage['description'];
+                    ?>
+
+                    <span>
+                        <img class="second-stage-cast" src="<?php echo esc_url($castImage_url); ?>"
+                            alt="<?php echo esc_attr($castImage_alt); ?>">
+                        <p class="second-stage-cast-caption">
+                            <?php echo esc_attr($castImage_caption); ?>
+                        </p>
+                        <p class="second-stage-cast-description">
+                            <?php echo esc_attr($castImage_description); ?>
+                        </p>
+                    </span>
+                    <?php
+                }
+                ?>
+            </div>
+        </div>
+        <?php
         }
     }
     ?>
