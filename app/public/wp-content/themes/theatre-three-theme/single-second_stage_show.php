a2800182185278e130e44126868734f519936d71
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-09-05 10:50:44 -0400

    updated css and dynamic uploading

--- app/public/wp-content/themes/theatre-three-theme/single-second_stage_show.php
+++ app/public/wp-content/themes/theatre-three-theme/single-second_stage_show.php
@@ -1,36 +1,29 @@
 <?php
 get_header();
 ?>
-<h1 class="post-title">THIS IS MADE BY THE SINGLE SECOND STAGE SHOW</h1>
-
-<div class="show-layout">
-    <div class="">
-    <?php
-    if (have_posts()) {
-        while (have_posts()) {
-            the_post();
-
-            // Get the image URL from the ACF field
-            $image = get_field('show_image');
-            $castImages = array();
-
-            // Loop through the cast_image_X fields
-            for ($i = 1; ; $i++) {
-                $castImage = get_field('cast_image_' . $i);
-
-                if ($castImage) {
-                    $castImages[] = $castImage;
-                } else {
-                    // Break the loop if no more images found
-                    break;
-                }
+<?php
+if (have_posts()) {
+    while (have_posts()) {
+        the_post();
+        $image = get_field('show_image');
+        $castImages = array();
+        for ($i = 1; ; $i++) {
+            $castImage = get_field('cast_image_' . $i);
+            if ($castImage) {
+                $castImages[] = $castImage;
+            } else {
+                break;
             }
-            ?>
+        }
+        ?>
 
-            <div class="show-header">
+        <div class="second-stage-content-columns">
+
+            <div class="second-stage-left-column">
                 <h1 class="post-title">
                     <?php the_title(); ?>
                 </h1>
+                <hr class="second-stage-title-line-break">
 
                 <?php
                 if ($image) {
@@ -48,15 +41,35 @@ get_header();
                 ?>
             </div>
 
-            <div class="show-content">
+            <div class="second-stage-middle-column">
                 <h2 class="post-title">About the Show</h2>
+                <hr class="second-stage-title-line-break">
                 <div class="generic-post-content">
                     <?php the_content(); ?>
                 </div>
+
+                <div class="second-stage-cast-container">
+                    <?php foreach ($castImages as $index => $castImage): ?>
+                        <div class="second-stage-cast-images">
+                            <span class="second-stage-cast-content">
+                                <img class="second-stage-cast" src="<?php echo esc_url($castImage['url']); ?>"
+                                    alt="<?php echo esc_attr($castImage['alt']); ?>">
+                                <p class="second-stage-cast-caption">
+                                    <?php echo esc_attr($castImage['caption']); ?>
+                                </p>
+                                <p class="second-stage-cast-description">
+                                    <?php echo esc_attr($castImage['description']); ?>
+                                </p>
+                            </span>
+                        </div>
+                    <?php endforeach; ?>
+                </div>
+
             </div>
 
-            <div class="show-metabox">
+            <div class="second-stage-right-column">
                 <h2 class="post-title">Ticket Information</h2>
+                <hr class="second-stage-title-line-break">
                 <div class="metabox-ticket-information-header">
                     <?php
                     $ticket_information = get_field('show_ticket_information');
@@ -72,35 +85,12 @@ get_header();
                     ?>
                 </div>
             </div>
-
-            <div class="second-stage-cast-images">
-                <?php
-                foreach ($castImages as $index => $castImage) {
-                    $castImage_url = $castImage['url'];
-                    $castImage_alt = $castImage['alt'];
-                    $castImage_caption = $castImage['caption'];
-                    $castImage_description = $castImage['description'];
-                    ?>
-
-                    <span>
-                        <img class="second-stage-cast" src="<?php echo esc_url($castImage_url); ?>"
-                            alt="<?php echo esc_attr($castImage_alt); ?>">
-                        <p class="second-stage-cast-caption">
-                            <?php echo esc_attr($castImage_caption); ?>
-                        </p>
-                        <p class="second-stage-cast-description">
-                            <?php echo esc_attr($castImage_description); ?>
-                        </p>
-                    </span>
-                    <?php
-                }
-                ?>
-            </div>
         </div>
         <?php
-        }
     }
-    ?>
+}
+?>
+
 </div>
 
 <?php
