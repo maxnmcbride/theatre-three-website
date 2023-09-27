ae0cc297f35fa3e096b0d42051a1377c2850b3d8
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-09-27 16:30:18 -0400

    fixing footer

--- app/public/wp-content/themes/theatre-three-theme/footer.php
+++ app/public/wp-content/themes/theatre-three-theme/footer.php
@@ -3,7 +3,7 @@
   <div class="footer-column">
 
     <h4 class="footer-column-heading">Contact Us</h4>
-    <hr>
+    <hr class = "footer-column-hr">
     <h5 class="footer-column-heading">Location</h5>
     <pre class="footer-column-paragraph">
       <a class="address" href="https://www.google.com/maps/search/412-main-street-port-jefferson-ny" target="_blank">
@@ -31,7 +31,7 @@
 
   <div class="footer-column">
     <h4 class="footer-column-heading">Hours</h4>
-    <hr>
+    <hr class = "footer-column-hr">
     <pre class="footer-column-paragraph">
     <h5 class="footer-column-heading">Box Office</h5>
       <i>Mon - Sat</i> 10AM - 5PM
