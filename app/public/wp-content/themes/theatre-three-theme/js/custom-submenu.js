1313af344f852a660f58e1bf1d36215c1ae7d2ea
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-12-21 22:36:24 -0500

    updating nav, with interesting outcome. not exactly what i have in mind, but interesting and getting there

--- app/public/wp-content/themes/theatre-three-theme/js/custom-submenu.js
+++ app/public/wp-content/themes/theatre-three-theme/js/custom-submenu.js
@@ -1,19 +1,30 @@
-jQuery(document).ready(function($) {
-    // Prevent the default action on click
-    $('.nav-item > a').click(function(event) {
-      event.preventDefault();
-  
-      // Close all open sub-menus
-      $('.sub-links').not($(this).next()).slideUp();
-  
-      // Toggle the next submenu
-      $(this).next('.sub-links').slideToggle();
-    });
-  
-    // Close sub-menu when clicking anywhere else on the document
-    $(document).click(function(event) {
-      if (!$(event.target).closest('.nav-item').length) {
-        $('.sub-links').slideUp();
-      }
-    });
+jQuery(document).ready(function ($) {
+  // Toggle sub-menus on nav item click
+  $('.nav-item > a').click(function (event) {
+    event.preventDefault(); // Prevent following the link
+
+    // Get the next sibling sub-menu
+    var submenu = $(this).next('.sub-links');
+
+    // Check if the submenu is currently displayed
+    if (submenu.is(':hidden')) {
+      // Close other sub-menus
+      $('.sub-links').slideUp().css('display', 'none');
+
+      // Show this submenu with flex
+      submenu.slideDown(function () {
+        $(this).css('display', 'flex');
+      });
+    } else {
+      // If it is displayed, slide it up
+      submenu.slideUp();
+    }
   });
+
+  // Close all sub-menus if clicking outside of nav
+  $(document).click(function (event) {
+    if (!$(event.target).closest('.nav-item').length) {
+      $('.sub-links').slideUp().css('display', 'none');
+    }
+  });
+});
