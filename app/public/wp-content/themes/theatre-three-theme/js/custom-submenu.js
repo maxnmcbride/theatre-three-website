0cd894dfe3eb98cb701a97af56998e6e1bc008fb
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2023-12-31 17:02:42 -0500

    working on updating site, nav bar working well

--- app/public/wp-content/themes/theatre-three-theme/js/custom-submenu.js
+++ app/public/wp-content/themes/theatre-three-theme/js/custom-submenu.js
@@ -1,30 +1,32 @@
-jQuery(document).ready(function ($) {
-  // Toggle sub-menus on nav item click
-  $('.nav-item > a').click(function (event) {
-    event.preventDefault(); // Prevent following the link
+// This was re-written and combined with the JS file
 
-    // Get the next sibling sub-menu
-    var submenu = $(this).next('.sub-links');
+// jQuery(document).ready(function ($) {
+//   // Toggle sub-menus on nav item click
+//   $('.nav-item > a').click(function (event) {
+//     event.preventDefault(); // Prevent following the link
 
-    // Check if the submenu is currently displayed
-    if (submenu.is(':hidden')) {
-      // Close other sub-menus
-      $('.sub-links').slideUp().css('display', 'none');
+//     // Get the next sibling sub-menu
+//     var submenu = $(this).next('.sub-links');
 
-      // Show this submenu with flex
-      submenu.slideDown(function () {
-        $(this).css('display', 'flex');
-      });
-    } else {
-      // If it is displayed, slide it up
-      submenu.slideUp();
-    }
-  });
+//     // Check if the submenu is currently displayed
+//     if (submenu.is(':hidden')) {
+//       // Close other sub-menus
+//       $('.sub-links').slideUp().css('display', 'none');
 
-  // Close all sub-menus if clicking outside of nav
-  $(document).click(function (event) {
-    if (!$(event.target).closest('.nav-item').length) {
-      $('.sub-links').slideUp().css('display', 'none');
-    }
-  });
-});
+//       // Show this submenu with flex
+//       submenu.slideDown(function () {
+//         $(this).css('display', 'flex');
+//       });
+//     } else {
+//       // If it is displayed, slide it up
+//       submenu.slideUp();
+//     }
+//   });
+
+//   // Close all sub-menus if clicking outside of nav
+//   $(document).click(function (event) {
+//     if (!$(event.target).closest('.nav-item').length) {
+//       $('.sub-links').slideUp().css('display', 'none');
+//     }
+//   });
+// });
