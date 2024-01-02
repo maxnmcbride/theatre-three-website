e7e648f66924dde4424b6c819bcd237541615ca5
maxnmcbride@gmail.com <maxnmcbride@gmail.com>
2024-01-01 23:24:19 -0500

    added on page load delay for smoother UI

--- app/public/wp-content/mu-plugins/custom-post-types.php
+++ app/public/wp-content/mu-plugins/custom-post-types.php
@@ -82,25 +82,25 @@ function custom_post_types()
         )
     );
 
-// AUDTIONS
+    // AUDTIONS
 
-register_post_type(
-    'auditions',
-    array(
-        'rewrite' => array('slug' => 'auditions'),
-        'has_archive' => true,
-        'show_in_rest' => true,
-        'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'custom-fields'),
-        'public' => true,
-        'menu_icon' => 'dashicons-megaphone',
-        'labels' => array(
-            'name' => 'Auditions',
-            'singular_name' => 'Audition',
-            'add_new_item' => 'Add Audition',
-            'edit_item' => 'Edit Audition',
+    register_post_type(
+        'auditions',
+        array(
+            'rewrite' => array('slug' => 'auditions'),
+            'has_archive' => true,
+            'show_in_rest' => true,
+            'supports' => array('title', 'editor', 'excerpt', 'thumbnail', 'custom-fields'),
+            'public' => true,
+            'menu_icon' => 'dashicons-megaphone',
+            'labels' => array(
+                'name' => 'Auditions',
+                'singular_name' => 'Audition',
+                'add_new_item' => 'Add Audition',
+                'edit_item' => 'Edit Audition',
+            )
         )
-    )
-);
+    );
 
 }
 
---
+++ app/public/wp-content/themes/theatre-three-theme/generic_page.php
@@ -0,0 +1,12 @@
+<?php
+/* Template Name: Generic Page Template */
+get_header();
+?>
+<div class="generic-page-body-container">
+    <div class="generic-page-content">
+        <?php the_content(); ?>
+    </div>
+</div>
+<?php
+get_footer();
+?>
